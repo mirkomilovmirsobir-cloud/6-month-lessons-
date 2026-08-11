5181783c37617b0bb84a2c83ac4d3587eb17f9fe
mirkomilovmirsobir <mirkomilovmirsobir@gmail.com>
2026-08-11 09:12:13 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+// m1
+
+// let a =40;
+// a >= 10 && a <= 99 ? console.log("a soni ikki xonali") : console.log("a soni ikki xonali emas")
+// a % 2 === 0 ? console.log("a soni juft") : console.log("a soni toq");
+
+// m11
+
+//   let a =121;
+//  a >= 99 && a <= 999 ? console.log("a soni uch xonali") : console.log("a soni uch xonali emas")
+//  a % 2 === 1 ? console.log("a soni toq") : console.log("a soni juft");
+
+// m12
+
+// let A = 5;
+// let B = 12;
+// let C = -5;
+
+// (A + B === 0) || (B + C === 0) || (A + C === 0) ? console.log("bor") : console.log("yoq");
+
+// m13
+
+// let a = 3;
+// let b = 4;
+// let c = 6;
+// a<b && b < c ? console.log("raqamlar ozib boruvchi joylashgan") : console.log("raqamlar osimaydi")
+
+// m14
+
+// let a = 3;
+// let b = 2;
+// let c = 1;
+// a<b && b < c || a> b && b> c ? console.log("raqamlar kopatib borvot yoki kamayib borvoti") : console.log("raqamlar osib borvot yoki kamayib")
+
+// m15
+
+let a = 6;
+let b = 4;
+let c = 6;
+a == c ? console.log("raqamlar birxil") : console.log("raqamlar birxil emas");
